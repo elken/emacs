130f65a4a0b9bdc9628a257ff4c3947f26bf9a07
Ellis Kenyo <me@elken.dev>
2024-11-25 09:05:26 +0000

    Borrow Magit changes from Doom
    Magit is quite chunky and does annoying things sometimes, so we fix
    those with love from Doom Emacs

--- lisp/lkn-vcs.el
+++ lisp/lkn-vcs.el
@@ -21,7 +21,111 @@
 ;;; Code:
 
 
-(use-package magit)
+(use-package magit
+  :custom
+  (magit-auto-revert-mode nil)
+  :init
+  ;; Borrwed from Doom
+  ;;;###autoload
+  (defun magit-display-buffer-fn (buffer)
+    "Same as `magit-display-buffer-traditional', except...
+
+- If opened from a commit window, it will open below it.
+- Magit process windows are always opened in small windows below the current.
+- Everything else will reuse the same window."
+    (let ((buffer-mode (buffer-local-value 'major-mode buffer)))
+      (display-buffer
+       buffer (cond
+               ((and (eq buffer-mode 'magit-status-mode)
+                     (get-buffer-window buffer))
+		'(display-buffer-reuse-window))
+               ;; Any magit buffers opened from a commit window should open below
+               ;; it. Also open magit process windows below.
+               ((or (bound-and-true-p git-commit-mode)
+                    (eq buffer-mode 'magit-process-mode))
+		(let ((size (if (eq buffer-mode 'magit-process-mode)
+				0.35
+                              0.7)))
+                  `(display-buffer-below-selected
+                    . ((window-height . ,(truncate (* (window-height) size)))))))
+
+               ;; Everything else should reuse the current window.
+               ((or (not (derived-mode-p 'magit-mode))
+                    (not (memq (with-current-buffer buffer major-mode)
+                               '(magit-process-mode
+				 magit-revision-mode
+				 magit-diff-mode
+				 magit-stash-mode
+				 magit-status-mode))))
+		'(display-buffer-same-window))
+
+               ('(magit--display-buffer-in-direction))))))
+
+  (defun magit--display-buffer-in-direction (buffer alist)
+    "`display-buffer-alist' handler that opens BUFFER in a direction.
+
+This differs from `display-buffer-in-direction' in one way: it will try to use a
+window that already exists in that direction. It will split otherwise."
+    (let ((direction (or (alist-get 'direction alist)
+			 'right))
+          (origin-window (selected-window)))
+      (if-let (window (window-in-direction direction))
+          (unless magit-display-buffer-noselect
+            (select-window window))
+	(if-let (window (and (not (one-window-p))
+                             (window-in-direction
+                              (pcase direction
+				(`right 'left)
+				(`left 'right)
+				((or `up `above) 'down)
+				((or `down `below) 'up)))))
+            (unless magit-display-buffer-noselect
+              (select-window window))
+          (let ((window (split-window nil nil direction)))
+            (when (and (not magit-display-buffer-noselect)
+                       (memq direction '(right down below)))
+              (select-window window))
+            (display-buffer-record-window 'reuse window buffer)
+            (set-window-buffer window buffer)
+            (set-window-parameter window 'quit-restore (list 'window 'window origin-window buffer))
+            (set-window-prev-buffers window nil))))
+      (unless magit-display-buffer-noselect
+	(switch-to-buffer buffer t t)
+	(selected-window))))
+
+  :config
+  (setq transient-display-buffer-action '(display-buffer-below-selected)
+        magit-display-buffer-function #'magit-display-buffer-fn
+        magit-bury-buffer-function #'magit-mode-quit-window)
+  (defvar magit-stale-p nil)
+
+  (defun magit-revert-buffer (buffer)
+    "Revert the current buffer and update the modeline."
+    (with-current-buffer buffer
+      (kill-local-variable 'magit-stale-p)
+      (when (and buffer-file-name (file-exists-p buffer-file-name))
+	(if (buffer-modified-p (current-buffer))
+	    (when (bound-and-true-p vc-mode)
+	      (vc-refresh-state)
+	      (force-mode-line-update))
+	  (revert-buffer t t t)))))
+  
+  (defun magit-mark-stale-buffers-a (&rest _)
+    (dolist (buffer (buffer-list))
+      (when (buffer-live-p buffer)
+	(if (get-buffer-window buffer)
+	    (magit-revert-buffer buffer)
+	  (with-current-buffer buffer
+	    (setq-local magit-stale-p t))))))
+
+  (defun magit-revert-buffer-maybe-h ()
+    "Revert the buffer if out of date."
+    (when magit-stale-p
+      (magit-revert-buffer (current-buffer))))
+
+  (add-hook 'buffer-list-update-hook #'magit-revert-buffer-maybe-h)
+  (advice-add 'magit-checkout :after #'magit-mark-stale-buffers-a)
+  (advice-add 'magit-branch-and-checkout :after #'magit-mark-stale-buffers-a))
 
 (use-package magit-todos
   :after magit
@@ -77,7 +181,11 @@
   (:map transient-map
         ([escape] . transient-quit-one))
   :init
-  (setq transient-levels-file  (expand-file-name "transient/levels" no-littering-var-directory)
+  (setq transient-default-level 5
+	magit-diff-refine-hunk t
+	magit-save-repository-buffers nil
+	magit-revision-insert-related-refs nil
+	transient-levels-file  (expand-file-name "transient/levels" no-littering-var-directory)
         transient-values-file  (expand-file-name "transient/values" no-littering-var-directory)
         transient-history-file (expand-file-name "transient/history" no-littering-var-directory)
         transient-default-level 5))
