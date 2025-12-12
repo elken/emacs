c9923dbe2c5a685901081e84cc75be446288516b
Ellis Kenyo <me@elken.dev>
2025-12-12 10:53:30 +0000

    Ensure relative numbers are set only when display-line-numbers-mode is on
    Prevents some buffers getting numbers turned on by mistake

--- lisp/lkn-defaults.el
+++ lisp/lkn-defaults.el
@@ -399,8 +399,9 @@ The DWIM behaviour of this command is as follows:
   ;; Line numbers
   (prog-mode                    . display-line-numbers-mode)
   (help-mode                    . (lambda () (display-line-numbers-mode -1)))
-  (display-line-numbers-mode . (lambda ()
-                                 (setq-local display-line-numbers 'relative)))
+  (display-line-numbers-mode    . (lambda ()
+                                    (when display-line-numbers-mode
+                                      (setq-local display-line-numbers 'relative))))
   :bind
   (("C-x k"                    . kill-current-buffer)
    ("C-x C-k"                  . lkn/sudo-kill-current-buffer)
